dd918e20da5a3d3b53ba675313d4ab5a4cb3555c
Fabricio Suarez <fabricio.suarez@anahuac.mx>
2025-10-23 12:54:08 -0600

    Actualización con cambios recientes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const dotenv = require('dotenv').config
-const port = process.env.PORT || 5000
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5001
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 
